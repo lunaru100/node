7a67096f8eccb846a90a58d26200f758ff1af1fc
Lunaru <lunaruteyvat@gmail.com>
2023-10-03 10:40:25 +0200

    first commit

--- src/nodes/Node.java
+++ src/nodes/Node.java
@@ -1,4 +1,31 @@
 package nodes;
 
-public class Node {
+public class Node <T> {
+    T element;
+    Node <T> next;
+
+    public Node(T element) {
+        this.element = element;
+    }
+
+    public Node(T element, Node<T> next) {
+        this.element = element;
+        this.next = next;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
 }
